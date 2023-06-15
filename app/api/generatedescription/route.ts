3151ad6025cea6cffb3c367f2afccca164e7f3b8
Joshua Hargreaves <me@joshhargreaves.co.uk>
2023-06-15 23:36:43 +0100

    Add Job generator
    Squash

---
+++ app/api/generatedescription/route.ts
@@ -0,0 +1,30 @@
+import { OpenAIStream, OpenAIStreamPayload } from "@/utils/OpenAIStream";
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  runtime: "edge",
+};
+
+export async function POST(request: NextRequest) {
+  const { prompt } = (await request.json()) as {
+    prompt?: string;
+  };
+
+  if (!prompt) {
+    return new Response("No prompt in the request", { status: 400 });
+  }
+
+  const payload: OpenAIStreamPayload = {
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: prompt }],
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 500,
+    stream: true,
+    n: 1,
+  };
+  const stream = await OpenAIStream(payload);
+  return new Response(stream);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,162 @@
-import Image from 'next/image'
+"use client"; // This is a client component 
+import DropDown, { ToneType } from '@/components/DropDown';
+import { useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
 export default function Home() {
+  const [generatedDescription, setGeneratedDescription] = useState<string>("");
+  const [tone, setTone] = useState<ToneType>("Story");
+  const [post, setPost] = useState<string>("");
+
+  const [jobTitle, setJobTitle] = useState('');
+  const [industry, setIndustry] = useState('');
+  const [companyName, setCompanyName] = useState('');
+  const [salary, setSalary] = useState('');
+  const [keywords, setKeywords] = useState('');
+
+  // add more vibes as needed
+  const handlePrompt = () => {
+    let prompt = `I want you to act as a recruiter. I will provide some information about a job opening, and it will be your job to create an enganging job description. This will include a catchy opening paragraph, a responsibilities section, about us section and a skills required section.
+
+    The name of the hiring company is ${companyName}, the job title is ${jobTitle} and the company industry is ${industry}. These keywords will help create the description ${keywords}.`;
+    switch (tone) {
+      case "Story":
+        prompt + `It should be a story`;
+        break;
+      case "Formal":
+        prompt + `It should be Formal`;
+        break;
+      case "Fun":
+        prompt + `It should be Fun`;
+        break;
+      default:
+        break;
+    }
+    return prompt;
+  };
+
+  // function to send post to OpenAI and get response
+  const generateDescription = async (e: any) => {
+    if (!jobTitle) {
+      return null;
+    }
+    if (!industry) {
+      return null;
+    }
+    if (!companyName) {
+      return null;
+    }
+    if (!keywords) {
+      return null;
+    }
+
+    e.preventDefault();
+    setGeneratedDescription("");
+    const prompt = handlePrompt();
+    const response = await fetch("/api/generatedescription", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+      }),
+
+    });
+
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    // This data is a ReadableStream
+    const data = response.body;
+    if (!data) {
+      return;
+    }
+
+    const reader = data.getReader();
+    const decoder = new TextDecoder();
+    let done = false;
+
+    while (!done) {
+      const { value, done: doneReading } = await reader.read();
+      done = doneReading;
+      const chunkValue = decoder.decode(value);
+      const formattedChunk = chunkValue.replace(/\n/g, "<br>");
+      setGeneratedDescription((prev) => prev + formattedChunk);
+    }
+  };
+
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
+      <section className="py-10 lg:py-20 ">
+        <div className="px-4">
+          <div className='max-w-5xl mx-auto'>
+            <h1 className="sm:text-6xl text-4xl font-bold text-slate-900">
+              Generate A Job Description.
+            </h1>
+            <p className="sm:text-lg text-lg text-slate-600">
+              Reach Talent Faster.
+            </p>
+            <div className="flex flex-col md:flex-row w-full p-12">
+              <div className='flex flex-col w-2/4 h-14'>
+                <div className="inline-block relative">
+                  <form>
+                    <div className="px-5 pb-5">
+                      <input placeholder="Job Title" required name="jobTitle" onChange={e => { setJobTitle(e.currentTarget.value); }} className=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-white-100  border border-gray-300 rounded-md shadow-inner"></input>
+                      <input placeholder="Industry" required onChange={e => { setIndustry(e.currentTarget.value); }} className=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-white-100  border border-gray-300 rounded-md shadow-inner"></input>
+                      <input placeholder="Company Name" required onChange={e => { setCompanyName(e.currentTarget.value); }} className=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-white-100  border border-gray-300 rounded-md shadow-inner"></input>
+                      <input placeholder="Salary" hidden type='number' className=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base   transition duration-500 ease-in-out transform border-transparent rounded-lg bg-white-100  border border-gray-300 rounded-md shadow-inner"></input>
+                      <div className="block">
+                        <DropDown tone={tone} setTone={setTone} />
+                      </div>
+                      <textarea max-Length="2000" required onChange={e => { setKeywords(e.currentTarget.value); }} placeholder="Keywords" className="text-black w-full h-56 px-4 py-2.5 mt-2 text-s bg-white border border-gray-300 rounded-md shadow-inner md:h-240" spellCheck="false"></textarea>
+                      <button
+                        className="bg-orange-600 rounded-md text-white font-medium px-4 py-2.5 sm:mt-2 mt-2 hover:bg-black/80 w-full"
+                        onClick={(e) => generateDescription(e)}>
+                        Generate Job Description
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+              <div className='flex flex-col w-2/4 h-14'>
+                <div className="inline-block relative text-slate-900 w-full">
+                  <Toaster
+                    position="top-right"
+                    reverseOrder={false}
+                    toastOptions={{ duration: 2000 }}
+                  />
+                  {generatedDescription && (
+
+                    <div className="max-w-5xl my-4 mx-auto">
+                      <div
+                        className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+                        onClick={() => {
+                          navigator.clipboard.write([
+                            new ClipboardItem({
+                              "text/html": new Blob([generatedDescription], { type: "text/html" }),
+                            }),
+                          ]);
+                          toast("Post copied to clipboard", {
+                            icon: "📋",
+                          });
+                        }}
+                        key={generatedDescription}
+                      >
+                        <p className="text-slate-900" dangerouslySetInnerHTML={{ __html: generatedDescription }} />
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
     </main>
   )
 }
